fe7c720c2f9dcdfb3ff92db1e34da97e10740bd9
serdalakdogan <serdal.akdogan@misli.com>
2022-09-02 01:46:55 +0300

    instal.js and package.json düzenlemeleri yapıldı.

---
+++ src/install.js
@@ -0,0 +1,16 @@
+import ImageZoom from "./App.vue";
+
+const VueImageZoom = {
+    install(Vue) {
+        // Let's register our component globally
+        // https://vuejs.org/v2/guide/components-registration.html
+        Vue.component("hello-world", ImageZoom);
+    }
+};
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VueImageZoom);
+}
+
+export default VueImageZoom;
